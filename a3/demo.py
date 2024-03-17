6ab37393780b6c001c6a870cc33e05e1b8604734
Luqi8229 <163711306+Luqi8229@users.noreply.github.com>
2024-03-16 23:32:56 -0700

    Delete a3 directory

--- a3/demo.py
+++
@@ -1,8 +0,0 @@
-#Luqi Chen
-#luqic2@uci.edu
-#69278864
-
-import ds_client
-
-if __name__ == "__main__":
-    ds_client.send("168.235.86.101", 3021, "lil123", "doomed123", "Goodbye World", "doomed")
