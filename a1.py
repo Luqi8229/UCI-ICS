ce0541362fcf9faa7d5f396883d9a08062e63d10
Luqi Chen <luqichen8229@gmail.com>
2024-03-16 23:57:19 -0700

    adding a2 a3 as3

--- a1.py
+++ a1.py
@@ -17,21 +17,41 @@ def file_list(path):
 def directory_list(path):
     return [dir for dir in path.iterdir() if dir.is_dir()]
 
-def print_info(info):
-    for str in info:
-        if type(str) is list:
-            print_info(str)
-        else:
-            print(str)
-
 def select_filename(path, name):
     return [fil for fil in file_list(path) if fil.name == name]
     
 def select_extension(path, ext):
     if ext.startswith(".") is False:
         ext = "." + ext
-    # return [fil for fil in file_list(path) if fil.suffix == ext]
-    return Path('.').glob(f'**/*{ext}')
+    return [fil for fil in file_list(path) if fil.suffix == ext]
+    # return Path('.').glob(f'**/*{ext}')
+
+def convert_to_Path(str):
+    return Path(PurePath(str))
+
+def remove_list_info(ls, stride=1):
+    return [ls[i] for i in range(len(ls)) if i > (stride-1)]
+
+def check_if_dsu(path):
+    if path.suffix != ".dsu":
+        print("Unable to delete non-DSU files. Please input valid DSU file.")
+        return False
+    return True
+
+def check_if_directory_file(path:str):
+    path = convert_to_Path(path)
+    if path.is_dir() is True or path.is_file() is True:
+        return True
+    else:
+        print("Invalid directory.")
+        return False
+
+def print_info(info):
+    for str in info:
+        if type(str) is list:
+            print_info(str)
+        else:
+            print(str)
 
 def recursive_contents(path, options=[]):
     content = []
@@ -42,11 +62,11 @@ def recursive_contents(path, options=[]):
             content.append( path )
             content.append( file_list(path) )
         else:
-            if "f" in options:
+            if "-f" in options:
                 content.append( file_list(path) )
-            elif "s" in options:
+            elif "-s" in options:
                 content.append( select_filename(path, options[1]) )
-            elif "e" in options:
+            elif "-e" in options:
                 content.append( select_extension(path, options[1]) )
         if i == numDirectory:
             break
@@ -62,78 +82,78 @@ def default_contents(path):
 def list_contents(path, options):
     contents = []
     if len(options) == 0:
-        contents.append( default_contents(path) )
+        print("Here are your file contents:")
+        contents.append(default_contents(path))
     else:
         nOptions = remove_list_info(options)
         if options[0] == "-r":
+            print("Here are all your files and directories:")
             if len(nOptions) > 0:
-                contents.append( recursive_contents(path, nOptions) )
+                contents.append(recursive_contents(path, nOptions))
             else:
-                contents.append( recursive_contents(path) )
+                contents.append(recursive_contents(path))
         elif options[0] == "-f":
-            contents.append( file_list(path) )
+            print("Here are all your files:")
+            contents.append(file_list(path))
         elif options[0] == "-s":
-            contents.append( select_filename(path, options[1]) )
+            print(f"Here are all your {options[1]} files:")
+            contents.append(select_filename(path, options[1]))
         elif options[0] == "-e":
-            contents.append( select_extension(path, options[1]) )
+            print(f'Here are all your {options[1]} files')
+            contents.append(select_extension(path, options[1]))
+                
+    contents = remove_input_directory(path, contents)
     print_info(contents)
 
-def convert_to_Path(str):
-    return Path(PurePath(str))
-
-def remove_list_info(ls, stride=1):
-    return [ls[i] for i in range(len(ls)) if i > (stride-1)]
+def remove_input_directory(path, contents:list):
+    newContents = []
+    for info in contents:
+        if info == path:
+            continue
+        if type(info) is list:
+            newContents.append(remove_input_directory(path, info))
+        else:
+            newContents.append(info)
+    return newContents
 
 def create_new_file(path, input):
-    if input[0] != "-n":
-        print("Incorrect feature; must be -n (name)")
-        return
-    else:
-        file_name = input[1] + ".dsu"
-        file = path / file_name
-        file.touch()
+    assert (input[0] == "-n"), "Incorrect function feature: must be -n (name)"
+    assert len(input) > 0, "missing -n or name"
+
+    file_name = input[1] + ".dsu"
+    file = path / file_name
+    file.touch()
         
 def delete_DSU_file(path):
     if check_if_dsu(path) is True:
         path.unlink()
         print(path, "DELETED")
         
-def print_dsu_content(path):
-    file = path.parts[-1]
+def read_file_contents(path):
+    fileName = path.name
     
     if path.stat().st_size == 0:
         print("EMPTY")
         return
 
-    with open(file, "r") as f:
+    with open(fileName, "r") as f:
         for line in f.readlines():
             print(line)
 
-def check_if_dsu(path):
-    if path.suffix != ".dsu":
-        print("ERROR: not a dsu file, try again")
-        return False
-    return True
-
-def check_if_directory_file(path):
-    if path.is_dir() is True or path.is_file() is True:
-        return
-    else:
-        print("ERROR: no directory given")
-
 def run_command(ls):
     path = convert_to_Path( ls[1] )
-    check_if_directory_file(path)
-    nls = remove_list_info(ls, 2)
+    if check_if_directory_file(path) is True:
+        nls = remove_list_info(ls, 2)
 
-    if ls[0].startswith("L"):
-        list_contents(path, nls)
-    elif ls[0].startswith("C"):
-        create_new_file(path, nls)
-    elif ls[0].startswith("D"):
-        delete_DSU_file(path)
-    elif ls[0].startswith("R"):
-        print_dsu_content(path)
+        if ls[0] == "L":
+            list_contents(path, nls)
+        elif ls[0] == "D":
+            delete_DSU_file(path)
+
+        elif ls[0] == "C":
+            create_new_file(path, nls)
+        elif ls[0] == "R":
+            read_file_contents(path)
     
 def getInput():
     userInput = input()
