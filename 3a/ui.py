8a7f255f6b5ec3eaa4bf09e4690e1ea3655c5421
Luqi Chen <luqichen8229@gmail.com>
2024-03-17 16:51:31 -0700

    menus added

---
+++ 3a/ui.py
@@ -0,0 +1,31 @@
+
+
+##################### MENUS ##########################
+
+def run_M_menu():
+    print(f"{"Main Menu":-^50}",
+          "\n list | list contents from a directory",
+          "\n DSU | manage a DSU file",
+          "\n publish | publish to the ICS 32 server",
+          "\n menu | to print this menu",
+          "\n q | quit",
+          )
+    
+def run_L_menu():
+    print("\nListing options",
+          "\n all | to list all files and directories",
+          "\n files | to list only files",
+          "\n name | to list a specific file name",
+          "\n ext | to list files under a specific extension",
+          "\n menu | to print this menu",
+          )
+    
+def run_R_menu():
+    print("\nMenu",
+          "\n files | list all files",
+          "\n name | list all files under specific file name",
+          "\n ext | list all specific extensions",
+          "\n menu | print this menu",
+          )
+
+
