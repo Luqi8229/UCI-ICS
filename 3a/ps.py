1bf75d625e8418f8c51a3bbc049a283bedb8bf19
Luqi Chen <luqichen8229@gmail.com>
2024-03-17 16:52:20 -0700

    basic file functions added

---
+++ 3a/ps.py
@@ -0,0 +1,20 @@
+
+from pathlib import Path
+
+def file_list(path):
+    return [fil for fil in path.iterdir() if fil.is_file()]
+
+def directory_list(path):
+    return [dir for dir in path.iterdir() if dir.is_dir()]
+
+def select_filename(path, name):
+    return [fil for fil in file_list(path) if fil.name == name]
+    
+def select_extension(path, ext):
+    if ext.startswith(".") is False:
+        ext = "." + ext
+    return [fil for fil in file_list(path) if fil.suffix == ext]
+
+def path(str):
+    return Path(str)
+
